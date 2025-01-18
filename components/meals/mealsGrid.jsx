2b75c5ad7e2da89a94b0becbf135ac7289f83b57
Parsa_Shabanpour <parsash1380@gmail.com>
2025-01-18 11:01:52 +0330

    implement the meals components

---
+++ components/meals/mealsGrid.jsx
@@ -0,0 +1,14 @@
+import classes from "./mealsGrid.module.css";
+import MealItem from "@/components/meals/mealItem";
+
+export default function MealsGrid({ meals }) {
+  return (
+    <ul className={classes.meals}>
+      {meals.map((meal) => (
+        <li key={meal.id}>
+          <MealItem {...meal} />
+        </li>
+      ))}
+    </ul>
+  );
+}
